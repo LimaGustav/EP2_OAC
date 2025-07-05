753a72a9e99dec4722947965d1abef2ce12457ec
Richard Nascimento <rcdwoods@gmail.com>
2025-07-05 17:58:18 -0300

    :hammer: Adjust Bubblesort to use float instead of integer

--- BubbleSort/MIPS/bubblesort.asm
+++ BubbleSort/MIPS/bubblesort.asm
@@ -1,17 +1,21 @@
 .data
-	vetor: .word 1 12 3 5 3 7 0 -15 11
+	vetor: .float 1.1 12.0 3.0 5.0 3.0 7.0 0.0 -15.0 11.0
+	zeroF: .float 0.0
 	space: .asciiz " "
 	end: .asciiz "\nFim do looping"
 	antes: .asciiz "Antes do algoritmo: "
 	depois: .asciiz "\nDepois do algoritmo: "
+
 .text
 	la $s0, vetor
 	li $s1, 0 # i do loop
 	li $s3, 4 # tamanho da palavra
-	li $s4, 8 # ultimo valor de de i
-	li $s5, 7 # ultimo valor de j
+	li $s4, 9 # ultimo valor de de i
+	li $s5, 8 # ultimo valor de j
 	
 	mul $s6, $s4, $s3 # ultima posicao do vetor
+	
+	lwc1 $f10, zeroF
 	
 	
 	la $a0, antes
@@ -27,27 +31,23 @@
 	   li $s2, 0 # j do loop2
 	   
 	   loop2:
-	      beq $s2, $s5, end_loop2
+	      beq $s2, $s5, end_loop2 # checa se o j do loop2 é igual à última posição
 	      mul $t2, $s2, $s3 # posicao atual do j
 	      addi $t4, $s2, 1 # proximo valor de j
 	      mul $t4, $t4, $s3
 	      
-	      lw $t3, 0($s0) # lê valor da posicao j do vetor
-	      #move $a0, $t3
-	      
-	      # jal imprime_inteiro
+	      lwc1 $f12, 0($s0) # lê valor da posicao j do vetor
 	      
 	      addi $s2, $s2, 1
 	      addi $s0, $s0, 4
 	      
-	      lw $t5, 0($s0) # lê valor da proxima posicao j do vetor
+	      lwc1 $f14, 0($s0) # lê valor da proxima posicao j do vetor
 	      
 	      move $a0, $t2
-	      move $a1, $t3
-	      move $a2, $t4
-	      move $a3, $t5
+	      move $a1, $t4
 	      
-	      blt, $t5, $t3, swap
+	      c.lt.s, $f14, $f12
+	      bc1t swap
 	      
 	      j loop2
 	      
@@ -70,18 +70,18 @@
 	   syscall
 	   
 	   
-	swap: # $a0 = posicao à esquerda, $a1 = valor à esquerda; $a2 = posicao à direita; $a3 = valor à direita
+	swap: # $a0 = posicao à esquerda, $a1 = posicao à direita; $f12 = float a esquerda; $f14 = float a direita
 	   move $t0, $a0 # armazena posicao do valor a esquerda em registrado temporaria
-	   move $t1, $a1 # armazena valor a esquerda em registrador temporario
+	   mov.s $f1, $f12 # armazena valor a esquerda em registrador temporario
 	   
 	   la $t2, vetor
 	   add $t3, $t2, $a0 # posicao no vetor do valor a esquerda
 	   
 	   la $t4, vetor
-	   add $t5, $t4, $a2 # posicao no vetor do valor a direita
+	   add $t5, $t4, $a1 # posicao no vetor do valor a direita
 	   
-	   sw $a3, 0($t3) # coloca valor a direita na posicao da esquerda
-	   sw $t1, 0($t5) # coloca valor a esquerda na posicao da direita
+	   swc1 $f14, 0($t3) # coloca valor a direita na posicao da esquerda
+	   swc1 $f1, 0($t5) # coloca valor a esquerda na posicao da direita
 	   
 	   j loop2
 	   
@@ -131,7 +131,7 @@
               beq $t3, $t1, end_iv_loop
               lw $a0, 0($t2)
               
-              jal imprime_inteiro
+              jal imprime_float
               
               addi $t2, $t2, 4 # incrementa a posicao do vetor
               addi $t3, $t3, 4 # incrementa o i do loop
@@ -144,5 +144,22 @@
 	   
               jr $ra
               
-              
-              
+         
+        imprime_float:
+	   addi $sp, $sp, -4
+	   sw $ra, 0($sp)
+	   
+	   mtc1 $a0, $f2
+	   
+	   add.s $f12, $f10, $f2
+	
+	   li $v0, 2
+	   syscall 
+	   
+	   la $a0, space
+	   jal imprime_string
+	   
+	   lw $ra, 0($sp)
+	   addi $sp, $sp, 4
+	   
+	   jr $ra
